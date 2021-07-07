9b97ca51a9c4af98348af11524486e0951790796
Marcelo Hornillos <marcehornillos@gmail.com>
2021-07-08 00:24:07 +0200

    Reduce

---
+++ mapFilterReduce.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+lista = [1, 2, 3, 4]
+
+listaDobles = map(lambda x: x*2, lista)
+
+
+listaPares = filter(lambda x: x % 2 == 0, lista)
+
+sumatorio = reduce(lambda x, y: x + y, lista)
+
+suma100 = reduce(lambda x, y: x + y, range(101))
